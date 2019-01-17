13b4ecf5cf2156018915404db90d873924814713
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-17 10:04:16 -1000

    sort() method is buggy, most tests pass

--- matrix-generator.js
+++ matrix-generator.js
@@ -7,12 +7,18 @@ function matrixGenerator(rows, columns) {
     for (let j = 0; j < columns; j++) {
       const randomNumber = Math.round((Math.random() * 100));
 
-      newRow.push(randomNumber)
-      newRow.sort()
+      newRow.push(randomNumber);
+      newRow.sort();  // sorts by bitcode, buggy with numbers like 4 and 40, 5 and 50, etc
     }
-    
+
     matrix.push(newRow);
   }
+
+  const firstIndexes = [];
+
+  matrix.forEach(row => {
+    // firstIndexes.push(row[0]);
+  });
 
   console.log(matrix);
   return matrix;
